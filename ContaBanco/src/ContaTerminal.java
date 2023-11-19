24d5a99eb4fe5eb846f9ab5259f2beba2484bd76
jeconias30 <jeconiasdesign@gmail.com>
2023-11-19 17:21:53 -0300

    Meu projeto, conta banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,11 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO : Conhecer e importar as classes Scanner
+
+        // Exibir as mensagens para o nosso Usuario
+
+        // Obter pela Scanner os valores digitados no terminal.
+
+        // exibir a mensagem conta criada.
     }
 }
