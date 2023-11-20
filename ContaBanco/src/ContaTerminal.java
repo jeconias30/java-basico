e4b8d4a5d4b112dcc9be2f6ff7e654edca10b280
jeconias30 <jeconiasdesign@gmail.com>
2023-11-19 21:43:40 -0300

    desafio concluido parte 1
    terminei o desafio em menos tempo que pensei, agora é focar para fazer um upgrade neste projeto.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         //TODO : Conhecer e importar as classes Scanner
@@ -7,5 +9,42 @@ public class ContaTerminal {
         // Obter pela Scanner os valores digitados no terminal.
 
         // exibir a mensagem conta criada.
+        Scanner usuario = new Scanner(System.in);
+        int numero;
+        String agencia, nomeCliente;
+        double saldo;
+        
+
+        System.out.println("----Vamos criar uma conta----");
+        System.out.println("------------------------------");
+        System.out.println("");
+        System.out.println("Por favor, diga seu nome: ");
+        // Nome do novo usuário
+        nomeCliente = usuario.next();
+        System.out.println("Por favor, digite o número da agência: ");
+        //informando o numero da agencia
+        agencia = usuario.next();
+        System.out.println("Informe o número de usuário: ");
+        //informando o numero da conta
+        numero = usuario.nextInt();
+
+        System.out.println("======================================");
+        System.out.println("======================================");
+        System.out.println("======================================");
+        System.out.println("======= Muito bem "+nomeCliente+" =========");
+        System.out.println("======================================");
+        System.out.println("======================================");
+        System.out.println("======================================");
+        System.out.println("Para finalizar deposite qualquer valor para render 110% no CDI");
+
+        //colocando um valor para ter saldo positivo em conta
+        System.out.println("Isira o valor R$ ");
+        saldo = usuario.nextDouble();
+        System.out.println("----CONTA FEITA COM SUCESSO----");
+        System.out.println("================");
+        System.out.println("================");
+        System.out.println("================");
+        // Mostrando na tela os dados criados.
+        System.out.println(nomeCliente+" Sua agencia é "+agencia+" de número "+numero+" com um saldo em conta: R$"+saldo);
     }
 }
