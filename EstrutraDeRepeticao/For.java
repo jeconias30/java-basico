4b2d29f07e358fbfc1d21a91dc62b70f91dec669
jeconias30 <jeconiasdesign@gmail.com>
2023-11-25 09:07:09 -0300

    inicio em estrtura de repeticao

---
+++ EstrutraDeRepeticao/For.java
@@ -0,0 +1,17 @@
+public class For {
+    public static void main(String[] args) {
+        // For (para)
+        // While (enquanto)
+        // Do While (faça enquanto)
+/*Estrutura do fluxo de controle 
+ * 
+ * For (bloco de inicializaçao; expressao booleana de validaçao; bloco de atualizaçao )
+ * {
+ *    comando que sera executado ate que a 
+ *    expressao de validaçao torne-se falsa   
+ * 
+ * }
+*/
+
+    }
+}
