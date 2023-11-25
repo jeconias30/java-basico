028dc289b524531f1d7599e85f136e6326bd8460
jeconias30 <jeconiasdesign@gmail.com>
2023-11-25 11:23:12 -0300

    concluindo a estrutura de repeticao "For"

---
+++ EstrutraDeRepeticao/for/BreakContinue.java
@@ -0,0 +1,12 @@
+public class BreakContinue {
+    public static void main(String[] args) {
+        for(int numero = 1; numero <= 5; numero ++){
+            if(numero == 3)
+                break; // tambem podemos usar o continue
+                       // Continue so muda o fluxo.
+            System.out.println(numero);
+        }
+
+        System.out.println("Parou!");
+    }
+}
